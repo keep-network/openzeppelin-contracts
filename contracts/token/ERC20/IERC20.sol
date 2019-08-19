1cc48a3891709b7d5e1c7699cb18e3156625775c
Nicholas Evans <nicholas.evans@thesis.co>
2019-08-19 16:46:26 +0300

    Make IERC20 event params indexed

--- contracts/token/ERC20/IERC20.sol
+++ contracts/token/ERC20/IERC20.sol
@@ -66,11 +66,11 @@ interface IERC20 {
      *
      * Note that `value` may be zero.
      */
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 indexed value);
 
     /**
      * @dev Emitted when the allowance of a `spender` for an `owner` is set by
      * a call to {approve}. `value` is the new allowance.
      */
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 indexed value);
 }
